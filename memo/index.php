685e050d1ca09de6403915dfc1009acbf130354e
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-04-10 16:29:52 +0900

    ページネーションのリンクを作成する処理

--- memo/index.php
+++ memo/index.php
@@ -1,6 +1,12 @@
 <?php
 require('dbconnect.php');
 
+/*最大ページ数を求める */
+$counts = $db->query('select count(*) as cnt from memo');
+$count = $counts->fetch_assoc();
+$max_page = floor(($count['cnt']+1)/5+1);
+echo $max_page;
+
 $stmt = $db->prepare('select * from memo order by id desc limit ?, 5');
 if(!$stmt) {
 	die($db->error);
@@ -39,5 +45,14 @@ $stmt->execute();
 	</div>
 	<hr>
 	<?php endwhile; ?>
+
+	<p>
+		<?php if ($page>1): ?>
+			<a href="?page=<?php echo $page-1; ?>"><?php echo $page-1; ?>ページ目へ</a> |
+		<?php endif; ?>
+		<?php if($page<$max_page): ?>
+			<a href="?page=<?php echo $page+1; ?>"><?php echo $page+1; ?>ページ目へ</a>
+		<?php endif; ?>
+	</p>
 </body>
 </html>
