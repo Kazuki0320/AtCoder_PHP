bfe1202d4a850adcfa9d90869bfde4dfd46c53cd
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-04-10 15:03:17 +0900

    一覧のページネーションの作成処理

--- memo/index.php
+++ memo/index.php
@@ -1,10 +1,13 @@
 <?php
 require('dbconnect.php');
 
-$memos = $db->query('select * from memo order by id desc');
-if (!$memos) {
+$stmt = $db->prepare('select * from memo order by id desc limit ?, 5');
+if(!$stmt) {
 	die($db->error);
 }
+$page = 5;
+$stmt->bind_param('i', $page);
+$stmt->execute();
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -19,10 +22,11 @@ if (!$memos) {
 
 	<p>→<a href="input.html">新しいメモ</a></p>
 
-	<?php while($memo = $memos->fetch_assoc()):?>
+	<?php $stmt->bind_result($id, $memo, $created); ?>
+	<?php while($stmt->fetch()):?>
 	<div>
-		<h2><a href="memo.php?id=<?php echo $memo['id'];?>"><?php echo htmlspecialchars(mb_substr($memo['memo'], 0, 50));?> </a></h2>
-		<time><?php echo htmlspecialchars($memo['created']);?></time>
+		<h2><a href="memo.php?id=<?php echo $id;?>"><?php echo htmlspecialchars(mb_substr($memo, 0, 50));?> </a></h2>
+		<time><?php echo htmlspecialchars($created);?></time>
 	</div>
 	<hr>
 	<?php endwhile; ?>
