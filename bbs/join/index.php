96b338d7503457bb65a7c142611a420f72c52108
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-04-16 14:21:23 +0900

    書き直しで戻ってきた場合の処理

--- bbs/join/index.php
+++ bbs/join/index.php
@@ -2,11 +2,16 @@
 session_start();
 require('../library.php');
 
-$form = [
-    'name' => '',
-    'email' => '',
-    'password' => '',
-];
+if(isset($_GET['action']) && $_GET['action'] === 'rewrite' && isset($_SESSION['form'])) {
+    $form = $_SESSION['form'];
+} else {
+    $form = [
+        'name' => '',
+        'email' => '',
+        'password' => '',
+    ];
+}
+
 $error = [];
 
 //下記はformが送信された時の処理
