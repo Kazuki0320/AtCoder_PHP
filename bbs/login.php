91c17821e163ea1b86efa78859c38cebfe24c445
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-04-16 18:31:16 +0900

    ログインのエラーメッセージ処理

--- bbs/login.php
+++ bbs/login.php
@@ -10,6 +10,28 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
     if($email === '' || $password === '') {
         $error['login'] = 'blank';
+    } else {
+        //ログインチェック
+        $db = dbConnect();
+        $stmt = $db->prepare('select id, name, password from members where email=? limit 1');
+        if(!$stmt) {
+            die($db->error);
+        }
+
+        $stmt->bind_param('s', $email);
+        $success = $stmt->execute();
+        if(!$success) {
+            die($db->error);
+        }
+
+        $stmt->bind_result($id, $name, $hash);
+        $stmt->fetch();
+
+        if(password_verify($password, $hash)) {
+            //ログイン成功
+        } else {
+            $error['login'] = 'failed';
+        }
     }
 }
 ?>
@@ -41,7 +63,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <?php if(isset($error['login']) && $error['login'] === 'blank'): ?>
                         <p class="error">* メールアドレスとパスワードをご記入ください</p>
                     <?php endif; ?>
-                    <p class="error">* ログインに失敗しました。正しくご記入ください。</p>
+                    <?php if(isset($error['login']) && $error['login'] === 'failed'): ?>
+                        <p class="error">* ログインに失敗しました。正しくご記入ください。</p>
+                    <?php endif; ?>
                 </dd>
                 <dt>パスワード</dt>
                 <dd>
