7a5cfa2d30c8d6333d4893bde3c82a5c95cb2498
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-05-25 10:04:54 +0900

    staticメソッド共通化について

--- phpReference/static09.php
+++ phpReference/static09.php
@@ -2,25 +2,54 @@
 	//staticプロパティの設定例
 	//[クラスを直接呼び出した場合]
 	
-	class testClass	{
-		public $test = "テスト";
-		public static $staticTest = 'staticなテスト';
+	// class testClass	{
+	// 	public $test = "テスト";
+	// 	public static $staticTest = 'staticなテスト';
+	// }
+
+	// //クラス外からの呼び出し
+	// echo testClass::$staticTest;//staticなテスト,スコープ定義演算子（::）
+	// echo testClass::$test;//error,インスタンスを生成していないので、存在しない
+
+	// //[インスタンスを生成して呼び出した場合]
+	// class testClass1 {
+	// 	public $test1 = "テスト1";
+	// 	public static $staticTest = "staticなテスト1";
+	// }
+
+	// //クラス外でインスタンスを生成
+	// $testInstance = new testClass1;
+	// echo $testInstance->test1;//テスト1
+
+	// echo $testInstance->staticTest;//インスタンスの中には、staticTestは存在しない
+
+	//[staticプロパティが、共通化してることを確認するメソッド]
+	/**
+	 * staticがついてないメソッドなどは、クラスのオブジェクト（インスタンス）を作成した段階で作られる。→クラスのオブジェクト毎に管理されている変数。
+	 * staticプロパティなどは、最初に呼び出されたタイミングで、作られる。→そのため、クラスのインスタンスに関係なく、共通化している。
+	 */
+	class NumPrint {
+		//プロパティ
+		public $num = 0;
+		//staticプロパティ
+		public static $staticNum = 0;
+		public function addNum() {
+			//プロパティを加算
+			$this->num++;
+			//staticプロパティを加算
+			self::$staticNum++;
+		}
 	}
 
-	//クラス外からの呼び出し
-	echo testClass::$staticTest;//staticなテスト
-	echo testClass::$test;//error,インスタンスを生成していないので、存在しない
+	$numPrint = new NumPrint();
+	$numPrint->addNum();
 
-	//[インスタンスを生成して呼び出した場合]
-	class testClass1 {
-		public $test1 = "テスト1";
-		public static $staticTest = "staticなテスト1";
-	}
+	print "プロパティの値".$numPrint->num;
+	print ",staticプロパティの値".NumPrint::$staticNum;
+	
+	$numPrint1 = new NumPrint();
+	$numPrint1->addNum();
 
-	//クラス外でインスタンスを生成
-	$testInstance = new testClass1;
-	echo $testInstance->test1;//テスト1
-
-	echo $testInstance->staticTest;//インスタンスの中には、staticTestは存在しない
-
+	print "<br>プロパティの値".$numPrint->num;//クラスのオブジェクト毎に管理しているため、1が表示される
+	print ",staticプロパティの値".NumPrint::$staticNum;//staticプロパティは共通化してるため、加算されて2と表示される
 ?>
