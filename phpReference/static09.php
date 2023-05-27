ae4bd116fcc7dcbd0380e140be4cea2a50d558b7
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-05-27 11:19:50 +0900

    静的メソッドと動的メソッドについて

--- phpReference/static09.php
+++ phpReference/static09.php
@@ -20,7 +20,6 @@
 	// //クラス外でインスタンスを生成
 	// $testInstance = new testClass1;
 	// echo $testInstance->test1;//テスト1
-
 	// echo $testInstance->staticTest;//インスタンスの中には、staticTestは存在しない
 
 	//[staticプロパティが、共通化してることを確認するメソッド]
@@ -44,12 +43,61 @@
 	$numPrint = new NumPrint();
 	$numPrint->addNum();
 
-	print "プロパティの値".$numPrint->num;
-	print ",staticプロパティの値".NumPrint::$staticNum;
+	print "プロパティの値".$numPrint->num;//表示結果:1
+	print ",staticプロパティの値".NumPrint::$staticNum;//表示結果:1
 	
 	$numPrint1 = new NumPrint();
 	$numPrint1->addNum();
 
 	print "<br>プロパティの値".$numPrint->num;//クラスのオブジェクト毎に管理しているため、1が表示される
 	print ",staticプロパティの値".NumPrint::$staticNum;//staticプロパティは共通化してるため、加算されて2と表示される
+
+	/**
+	 * 静的メソッドと動的メソッドにおけるselfとthisの違い
+	 */
+	//[静的メソッドからプロパティへアクセスする場合]
+	//thisは、クラスのインスタンス化をしていないと使えない。自身のクラスのインスタンス化したオブジェクトを指しているため。
+	class tesClass {
+		private $strVar;
+		private static $staticVar;
+
+		public static function staticName($var) {
+			$this->strVar = $var;
+			echo $this->strVar;
+		}
+	}
+
+	testClass::staticName("スタティック");//出力結果:エラー
+
+	//thisは使えないが、selfだとクラスを指すため使える。
+	class testClass1 {
+		private $strVar;
+		private static $staticVar;
+
+		public static function staticName($var) {
+			self::$staticVar = $var;
+			echo self::$staticVar;
+		}
+	}
+
+	testClass1::staticName("スタティック");//出力結果:スタティック
+
+	//[動的メソッドからプロパティへアクセスする場合]
+	class testClass2 {
+		private $instVar;
+		private static $staticVar;
+
+		public function instMethod($val) {
+			$this->instVar = $val;
+			echo $this->instVar;
+
+			echo "\n";
+
+			self::$staticVar = $val;
+			echo self::$staticVar;
+		}
+	}
+
+	$obj = new testClass2;
+	$obj->instMethod("インスタンス");//出力結果:スタティック スタティック
 ?>
