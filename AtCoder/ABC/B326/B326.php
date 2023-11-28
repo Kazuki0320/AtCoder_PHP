3480cf796b4335a900701772441b5736f08309cc
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-11-28 10:18:16 +0900

    修正

--- AtCoder/ABC/B326/B326.php
+++ AtCoder/ABC/B326/B326.php
@@ -6,7 +6,7 @@ for ($i = 1; $i < 10; $i++) {
     for ($j = 0; $j < 10; $j++) {
 			for ($k = 0; $k < 10; $k++) {
 				$c = $i * 100 + $j * 10 + $k;
-				if ($number <= $c && $number * $j + $k) {
+				if ($number <= $c && $i * $j == $k) {
 					echo $c;
 					exit (0);
 				}
