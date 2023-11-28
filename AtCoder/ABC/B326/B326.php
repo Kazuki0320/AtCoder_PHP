5f1b446822852e85ce4a7199f32b38ea48732df2
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-11-28 10:11:51 +0900

    ABC:B326完了

---
+++ AtCoder/ABC/B326/B326.php
@@ -0,0 +1,16 @@
+<?php
+$number = trim(fgets(STDIN));
+$c = 0;
+
+for ($i = 1; $i < 10; $i++) {
+    for ($j = 0; $j < 10; $j++) {
+			for ($k = 0; $k < 10; $k++) {
+				$c = $i * 100 + $j * 10 + $k;
+				if ($number <= $c && $number * $j + $k) {
+					echo $c;
+					exit;
+				}
+			}
+    }
+}
+?>
