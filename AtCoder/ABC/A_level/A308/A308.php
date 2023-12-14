3d48fb1adcdb44f60774e23836c29add804800ab
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-12-14 23:03:16 +0900

    ABC:A308正しい解答

--- AtCoder/ABC/A_level/A308/A308.php
+++ AtCoder/ABC/A_level/A308/A308.php
@@ -1,11 +1,11 @@
 <?php
-	fscanf(STDIN, "%d", $n);
+	$n = explode(" ", trim(fgets(STDIN)));
 
-	for($i = 0; $i < 8; $i++) {
-		if($n[$i] < $n[$i] && ($n >= 100 && $n <= 675) && ($n % 25 == 0)) {
-			echo "Yes";
-		} else {
+	for($i = 0; $i < 7; $i++) {
+		if(!($n[$i] <= $n[$i + 1]) || !($n[$i] >= 100 && $n[$i] <= 675) || !($n[$i] % 25 == 0)) {
 			echo "No";
+			exit;
 		}
 	}
+	echo "Yes";
 ?>
