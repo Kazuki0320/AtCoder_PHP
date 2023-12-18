b9d598a8b6855698b08cb7b62b46c74f03102218
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-12-19 00:08:53 +0900

    ABC:A304完了

--- AtCoder/ABC/A_level/A304/A304.php
+++ AtCoder/ABC/A_level/A304/A304.php
@@ -1,11 +1,21 @@
 <?php
 	fscanf(STDIN, "%d", $n);
-	$people = trim(fgets(STDIN));
 
 	$list = [];
 	while($arr = trim(fgets(STDIN))){
-  $arr = explode(' ', $arr);
-  $list[] = $arr;
-}
-	var_dump($list);
+    $arr = explode(' ', $arr);
+    $list[] = ['name' => $arr[0], 'age' => (int)$arr[1]];
+    }
+	
+	//年齢が最も小さい人を見つける
+	$youngest = min(array_column($list, 'age'));
+	
+	//年齢が最も小さい人のインデックスを取得
+	$start_index = array_search($youngest, array_column($list, 'age'));
+	
+    //時計回りに順番に出力
+    for($i = 0; $i < $n; $i++) {
+        $index = ($start_index + $i) % $n;
+        echo $list[$index]['name'] . "\n";
+    }
 ?>
