a5a33b46c2b7a099b9495e5c6ee2764067c753b2
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-12-24 13:25:15 +0900

    ABC:B322完了

---
+++ AtCoder/ABC/B_level/B322/B322.php
@@ -0,0 +1,16 @@
+<?php
+  fscanf(STDIN, "%d %d", $N, $M);
+
+  $S = trim(fgets(STDIN));
+  $T = trim(fgets(STDIN));
+
+  if(strpos($T, $S) === 0 && strpos(strrev($T), strrev($S)) === 0) {
+    echo "0";
+  } else if(strpos($T, $S) === 0) {
+    echo "1";
+  } else if(strpos(strrev($T), strrev($S))) {
+    echo "2";
+  } else {
+    echo "3";
+  }
+?>
