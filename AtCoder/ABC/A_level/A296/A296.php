125033260d70ecb6fb729a5445d710315d8e7032
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-12-29 23:13:39 +0900

    一部修正

--- AtCoder/ABC/A_level/A296/A296.php
+++ AtCoder/ABC/A_level/A296/A296.php
@@ -1,13 +1,13 @@
 <?php
-	fscanf(STDIN, "%d", $N);
-	
-	$S = trim(fgets(STDIN));
+fscanf(STDIN, "%d", $N);
 
-	for($i = 0; $i < $N-1; $i++) {
-		if((($S[$i] == "M") && ($S[$i+1] == "M")) || (($S[$i] == "F") && ($S[$i+1] == "F"))) {
-			echo "No";
-			exit;
-		}
+$S = trim(fgets(STDIN));
+
+for($i = 0; $i < $N-1; $i++) {
+	if((($S[$i] == "M") && ($S[$i+1] == "M")) || (($S[$i] == "F") && ($S[$i+1] == "F"))) {
+		echo "No";
+		exit;
 	}
-	echo "Yes";
+}
+echo "Yes";
 ?>
