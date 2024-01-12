5a595cb075d37aa744a73a838a7eb12641968554
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-01-13 08:59:26 +0900

    ABC:B321未完了

--- AtCoder/ABC/B_level/B321/B321.php
+++ AtCoder/ABC/B_level/B321/B321.php
@@ -1,7 +1,18 @@
 <?php
 fscanf(STDIN, "%d %d", $N, $R);
 $number1 = explode(" ", trim(fgets(STDIN)));
-$number_s = sort($number);
-$number2 = preg_replace(max($number_s), "", $number_s);
-$number3 = preg_replace(min($number_s), "", $number_s);
+sort($number1);
+$newNumber = 0;
+for($i = 1; $i < $N-2; $i++) {
+    $newNumber += (int)$number1[$i];
+}
+$result = $R - $newNumber;
+
+if($result < 100) {
+    echo $result;
+} else if($result == 100){
+    echo 0;
+} else {
+    echo -1;
+}
 ?>
