dd0d7528a21f8e4ef70435064cd49725a699f481
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-04-30 23:27:28 +0900

    一部修正

--- AtCoder/ABC/B_level/B314/B314.php
+++ AtCoder/ABC/B_level/B314/B314.php
@@ -1,6 +1,7 @@
 <?php
     fscanf(STDIN, "%d", $number);
     $circleRatio = 1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+
     $arrayCircleRatio = str_split($circleRatio);
     for($i = 0; $i < $number; $i++) {
         echo $arrayCircleRatio[$i];
