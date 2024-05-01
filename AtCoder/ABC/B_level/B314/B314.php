9229731753ad3cccf7fccf3b5eb7c5e975e210c6
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-05-01 23:42:44 +0900

    ABC:B315失敗

--- AtCoder/ABC/B_level/B314/B314.php
+++ AtCoder/ABC/B_level/B314/B314.php
@@ -3,17 +3,18 @@
 $n = (int)trim(fgets(STDIN));
 
 $c = [];
+//各々の入力値を受け取る
 for($i =1; $i <= $n; $i++){
   $c[$i] = (int)trim(fgets(STDIN));
   $a[$i] = explode(" ", trim(fgets(STDIN)));
 }
 
 $x = (int)trim(fgets(STDIN));
-// array_map('intval', $a);
 
 $answer = [];
 $count = 0;
 
+//$xが含まれる配列の検索をして、要素の最小値を探す
 for($i =1; $i <= $n; $i++){
   if(in_array($x, $a[$i])){
 
