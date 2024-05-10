635db10ef73edd581ca14bd957b89575060bbb03
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-05-10 10:18:54 +0900

    修正

--- AtCoder/ABC/B_level/B303/B303.php
+++ AtCoder/ABC/B_level/B303/B303.php
@@ -6,20 +6,17 @@ for ($i = 1; $i < $n; $i++) {
         $array[] = strval($i).strval($j);
     }
 }
-var_dump($array);
+
 while ($line = trim(fgets(STDIN))) {
     $line = explode(" ", $line);
     for ($i = 0; $i < $n-1; $i++) {
         for ($j = 0; $j < count($array); $j++) {
             $num = $line[$i].$line[$i+1];
-            var_dump("num1:", $num);
             if ($line[$i] > $line[$i+1]) {
                 $num = $line[$i+1].$line[$i];
-                var_dump("num2:", $num);
             }
             if ($num === $array[$j]) {
                 array_splice($array, $j, 1);
-                var_dump("slice:", $array);
                 break;
             }
         }
