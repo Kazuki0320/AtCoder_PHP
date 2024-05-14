892fbfd1a7a7339789fa2a9e278e94d234a849b0
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-05-14 09:17:46 +0900

    ABC:B297完了

---
+++ AtCoder/ABC/B_level/B297/B297.php
@@ -0,0 +1,12 @@
+<?php
+$S = trim(fgets(STDIN));
+
+$first = strpos($S, "B");
+$last = strrpos($S, "B");
+
+if ($first % 2 != $last % 2 && preg_match("/[B].*[B]/", $S) && (preg_match("/R.*K.*R/", $S))) {
+    echo "Yes";
+} else {
+    echo "No";
+}
+
