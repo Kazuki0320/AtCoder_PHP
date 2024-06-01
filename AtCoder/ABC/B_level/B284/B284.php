df6ae820311ead1373da3e4d4e89fef56b31deb9
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-06-01 14:44:22 +0900

    ABC:B284完了

--- AtCoder/ABC/B_level/B284/B284.php
+++ AtCoder/ABC/B_level/B284/B284.php
@@ -1,1 +1,25 @@
 <?php
+    fscanf(STDIN, "%d", $A);
+
+    $num = [];
+    $imp = [];
+    for($i = 0; $i < $A; $i++) {
+        fscanf(STDIN, "%d", $B);
+        $num[] = $B;
+        $imp [] = array_map("intval", explode(" ", trim(fgets(STDIN))));
+    }
+
+    $result = [];
+    for($p = 0; $p < $A; $p++){
+        $n = 0;
+        for($j = 0; $j < $num[$p]; $j++) {
+                if($imp[$p][$j] % 2 == 1) {
+                        $n++;
+                }
+        }
+        $result [] = $n;
+    }
+    
+    foreach ($result as $value) {
+        echo $value . PHP_EOL;
+    }
