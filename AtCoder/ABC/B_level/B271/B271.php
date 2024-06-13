5ee7e177e47d547754cffcc9ee55bfbc28fa67a4
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-06-13 09:47:43 +0900

    ABC:B271完了

---
+++ AtCoder/ABC/B_level/B271/B271.php
@@ -0,0 +1,15 @@
+<?php
+    fscanf(STDIN, "%d%d", $A, $B);
+
+    
+    for($i = 0; $i < $A; $i++) {
+        $tmp = explode(" ", trim(fgets(STDIN)));
+        array_shift($tmp);
+        $ans[] = $tmp;
+    }
+
+    for($j = 0; $j < $B; $j++) {
+        list($s, $t) = explode(" ", trim(fgets(STDIN)));
+        echo $ans[$s-1][$t-1] . PHP_EOL;
+    }
+?>
