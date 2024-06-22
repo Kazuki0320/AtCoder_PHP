bf8f66ee99544ab4bdf3fac218fcfa740f98d5ca
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-06-22 12:26:25 +0900

    ABC:B261完了

--- AtCoder/ABC/B_level/B261/B261.php
+++ AtCoder/ABC/B_level/B261/B261.php
@@ -1,22 +1,29 @@
 <?php
-    fscanf(STDIN, "%d", $N);
+fscanf(STDIN, "%d", $N);
 
-    $inp = [];
+$inp = [];
 
-    for ($i = 0; $i < $N; $i++) {
-        $inp[$i] = str_split(trim(fgets(STDIN)));
-    }
-    // 行
-    for($i = 0; $i < $N; $i++) {
-        // 列
-        for($j = 0; $j < $N; $j++) {
-            if($i === $j) {
-                break;
-            } else if($inp[$i][$j] !== $inp[$j][$i]) {
-                echo "incorrect";
-                exit;
-            }
+// 入力を2次元配列として格納
+for ($i = 0; $i < $N; $i++) {
+    $inp[$i] = str_split(trim(fgets(STDIN)));
+}
+
+// 行
+for ($i = 0; $i < $N; $i++) {
+    // 列
+    for ($j = 0; $j < $N; $j++) {
+        if ($i === $j) {
+            continue; // 対角線上の要素は確認しない
+        }
+        $now = $inp[$i][$j];
+        if ($now == '-') {
+            continue;
+        } elseif ($now == 'L' && $inp[$j][$i] != 'W' || $now == 'W' && $inp[$j][$i] != 'L' || $now == 'D' && $inp[$j][$i] != 'D'){
+            echo 'incorrect';
+            exit;
         }
     }
+}
 
-    echo "correct";
+echo 'correct';
+?>
