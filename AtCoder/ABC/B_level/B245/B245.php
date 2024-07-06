2b2994cd1781e052e57577c70551d556f82488ca
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-07-06 16:10:37 +0900

    ABC:B245完了

--- AtCoder/ABC/B_level/B245/B245.php
+++ AtCoder/ABC/B_level/B245/B245.php
@@ -1,23 +1,21 @@
 <?php
-    fscanf(STDIN, "%d", $N);
+// 入力の読み取り
+fscanf(STDIN, "%d", $N);
 
-    $num = [];
-    for($i = 0; $i < $N; $i++) {
-        fscanf(STDIN, "%d", $A);
-        $num = $A;
+// 入力の数列を整数の配列に変換
+$num = array_map('intval', explode(" ", trim(fgets(STDIN))));
+
+// 重複を排除してソート
+$num = array_unique($num);
+sort($num);
+
+// 欠けている最小の非負整数を見つける
+for ($i = 0; $i <= count($num); $i++) {
+    // 配列の要素数が1つの場合の特別処理は不要
+    // iがnumの要素と一致しない場合、iを出力して終了
+    if (!in_array($i, $num)) {
+        echo $i;
+        break;
     }
+}
 
-    $num = array_unique($num);
-
-    for($i = 0; $i < $N; $i++) {
-        if(count($num) === 1) {
-            echo $i;
-            exit;
-        }
-
-        if($i === $num[$i]) {
-            break;
-        } else {
-            echo $i;
-        }
-    }
