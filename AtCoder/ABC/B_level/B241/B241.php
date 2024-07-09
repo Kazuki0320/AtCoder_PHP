83cb73c22ffcf0d4f17226cfaddced1225056c3d
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-07-09 22:18:20 +0900

    ABC:B241 正解コード

--- AtCoder/ABC/B_level/B241/B241.php
+++ AtCoder/ABC/B_level/B241/B241.php
@@ -1,32 +1,15 @@
 <?php
-    fscanf(STDIN, "%d%d", $N, $M);
 
-    $pasta_length = [];
-    $eat_day = [];
-    $flag = true;
+[$n,$m] = array_map('intval', explode(" ", trim(fgets(STDIN))));
+$a = array_map('intval', explode(" ", trim(fgets(STDIN))));
+$b = array_map('intval', explode(" ", trim(fgets(STDIN))));
 
-    // 麺の長さ
-    $pasta_length = array_map("intval",explode(" ", trim(fgets(STDIN))));
-
-    // 食事計画日数
-    $eat_day = array_map("intval", explode(" ", trim(fgets(STDIN))));
-    
-    for($i = 0; $i < $M; $i++){
-        if(!in_array($eat_day[$i], $pasta_length, true)) {
-            $flag = false;
-        }
-        
-        if($N < $M) {
-            $flag = false;
-        }
-        
-        if (count($eat_day) !== count(array_unique($eat_day))) {
-            $flag = false;
-        }
-    }
-
-    if($flag) {
-        echo "Yes";
+for ($i = 0; $i < $m; $i++) {
+    $idx = array_search($b[$i], $a);
+    if (($idx) === false) {
+        echo 'No' . PHP_EOL;exit;
     } else {
-        echo "No";
+        unset($a[$idx]);
     }
+}
+echo 'Yes'. PHP_EOL;
