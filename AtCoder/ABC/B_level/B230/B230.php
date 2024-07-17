c42249c0a41992a5ee2de56803a172314a648a0b
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-07-17 22:21:56 +0900

    一部修正

--- AtCoder/ABC/B_level/B230/B230.php
+++ AtCoder/ABC/B_level/B230/B230.php
@@ -1,11 +1,9 @@
 <?php
 // 入力を受け取る
 $S = trim(fgets(STDIN));
-
 // Tを作成する
 $pattern = 'oxx';
 $repeatedPattern = str_repeat($pattern, 105);
-
 // SがTの部分文字列かどうかをチェックする
 if (strpos($repeatedPattern, $S) !== false) {
     echo "Yes\n";
