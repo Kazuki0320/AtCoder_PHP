37af87e4fc357d3ccb88aaf2437cad1af852efd5
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-07-20 17:14:25 +0900

    ABC:B228 不正解

---
+++ AtCoder/ABC/B_level/B228/B228.php
@@ -0,0 +1,11 @@
+<?php
+list($n, $x) = explode(" ", trim(fgets(STDIN)));
+$a = explode(" ", trim(fgets(STDIN)));
+$b = [];
+
+for ($i = 0; $i < $n; $i++) {
+    $b[] = $x;
+    $x = $a[$x - 1];
+}
+
+echo count(array_unique($b));
