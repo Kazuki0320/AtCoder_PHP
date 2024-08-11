28f2b0823397e314b4aa31c70a55c304d0f44d94
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-08-11 22:42:00 +0900

    ABC:B197 incorrect

---
+++ AtCoder/ABC/B_level/B197/B197.php
@@ -0,0 +1,32 @@
+<?php
+fscanf(STDIN, "%d %d %d %d", $H, $W, $Y, $X);
+$S = [];
+for ($i = 0; $i < $H; $i++) {
+    $S[] = rtrim(fgets(STDIN));
+}
+var_dump($S);
+$ans = 1;
+$X--; $Y--;
+
+// 左
+for ($i = $X-1; $i >= 0; $i--) {
+    if ($S[$Y][$i] == '#') break;
+    $ans++;
+}
+// // 右
+for ($i = $X+1; $i < $W; $i++) {
+    if ($S[$Y][$i] == '#') break;
+    $ans++;
+}
+// // 上
+for ($i = $Y-1; $i >= 0; $i--) {
+    if ($S[$i][$X] == '#') break;
+    $ans++;
+}
+// // 下
+for ($i = $Y+1; $i < $H; $i++) {
+    if ($S[$i][$X] == '#') break;
+    $ans++;
+}
+
+print $ans;
