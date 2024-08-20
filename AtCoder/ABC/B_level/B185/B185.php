0d3592e61d50736600cbc6130b9eed7c16c554ec
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-08-20 15:45:24 +0900

    ABC:B185 incorrect

---
+++ AtCoder/ABC/B_level/B185/B185.php
@@ -0,0 +1,21 @@
+<?php
+fscanf(STDIN, "%d %d %d", $N, $M, $T);
+$Max = $N;
+$time = 0;
+for ($i = 0; $i < $M; $i++) {
+  fscanf(STDIN, "%d %d", $A, $B);
+  $N -= ($A - $time);
+  if ($N <= 0) {
+      echo "No";
+      exit;
+  }
+  $N = min($Max, $N + $B - $A);
+  $time = $B;
+}
+
+$N -= $T - $time;
+if ($N <= 0) {
+    echo "No";
+} else {
+    echo "Yes";
+}
