795add53c6a70d6ad98693ed8bce1349c4abaf3f
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-08-21 10:19:08 +0900

    ABC:B184 correct

---
+++ AtCoder/ABC/B_level/B184/B184.php
@@ -0,0 +1,17 @@
+<?php
+    fscanf(STDIN, "%d%d", $N, $X);
+
+    $S = str_split(trim(fgets(STDIN)));
+    foreach($S as $answer) {
+        if($answer == "o") {
+            $X += 1;
+        } else {
+            $X -= 1;
+        }
+
+        if($X < 0) {
+            $X = 0;
+        }
+    }
+
+    echo $X;
