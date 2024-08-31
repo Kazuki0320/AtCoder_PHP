5f1ffb6080e305af7e810c102c5213c85b4f1de2
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-08-31 21:28:38 +0900

    Refacted code to improve caluculation effinciency

--- AtCoder/ABC/B_level/B178/B178.php
+++ AtCoder/ABC/B_level/B178/B178.php
@@ -1,10 +1,5 @@
 <?php
     fscanf(STDIN, "%d%d%d%d", $A, $B, $C, $D);
 
-    $max_number = [];
-    $max_number = $A * $C;
-    $max_number = $A * $D;
-    $max_number = $B * $C;
-    $max_number = $B * $D;
-
-    echo max($max_number) . PHP_EOL;
+    echo max($A * $C, $A * $D, $B * $C, $B * $D) . PHP_EOL;
+?>
