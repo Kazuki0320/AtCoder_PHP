cc6d18dc5d2ec7d9953260cfe63bb1156fb184ea
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-04 10:09:29 +0900

    ABC:B171 correct

---
+++ AtCoder/ABC/B_level/B171/B171.php
@@ -0,0 +1,9 @@
+<?php
+    fscanf(STDIN, "%d%d", $A, $B);
+    $S = explode(" ", trim(fgets(STDIN)));
+
+    sort($S);
+    $min_values = array_slice($S, 0, $B);
+    $result = array_sum($min_values);
+
+    echo $result . PHP_EOL;
