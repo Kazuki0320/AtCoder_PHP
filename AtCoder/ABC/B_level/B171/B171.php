1ad33dea3744823777c1a979b0826b3fed40e844
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-05 10:06:09 +0900

    partial modification

--- AtCoder/ABC/B_level/B171/B171.php
+++ AtCoder/ABC/B_level/B171/B171.php
@@ -1,7 +1,6 @@
 <?php
     fscanf(STDIN, "%d%d", $A, $B);
     $S = explode(" ", trim(fgets(STDIN)));
-
     sort($S);
     $min_values = array_slice($S, 0, $B);
     $result = array_sum($min_values);
