a0d5973dd9c7b039f07fde3465be51d192f061c5
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2024-09-08 10:29:11 +0900

    B169 incorrect

---
+++ AtCoder/ABC/B_level/B169/B169.php
@@ -0,0 +1,15 @@
+<?php
+    fscanf(STDIN, "%d", $N);
+
+    $A = array_map("intval", explode(" ", trim(fgets(STDIN))));
+
+    $result = 1;
+    foreach ($A as $key => $value) {
+        $result *= $value;   
+    }
+
+    if(pow(10, 18) < $result) {
+        echo "-1" . PHP_EOL; 
+    } else {
+        echo $result . PHP_EOL;
+    }
